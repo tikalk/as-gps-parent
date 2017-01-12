c38461077014ca3076041f30fa51222ca5d0e640
Boris Komraz <bkomraz1@gmail.com>
2017-01-12 14:12:33 +0200

    Change services names

--- build_all.bash
+++ build_all.bash
@@ -1,7 +1,9 @@
-( cd as-analytics && pwd && mvn clean install & )
+( cd as-analytics && pwd && mvn clean install )
 
 for i in as-geocoder-facade  as-gps-service  as-notifications   as-device-manager  as-gps-firehose     as-management   as-segments-service ; do
-   ( cd $i && pwd && ./gradlew buildImage & ) 
+   ( cd $i && pwd && ./gradlew buildImage )
 done
 
-( cd gps-tracker-ui  && pwd && docker build . -t localhost:5000/tikal/gps-tracker-ui:latest --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy & )
+( cd gps-tracker-ui  && pwd && docker build . -t docker-registry.tikalk.com:5000/tikal/gps-tracker-ui:latest --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy )
+
+wait
