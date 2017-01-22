fc52c64943af3b1bd3ad72aea5bb88c330bc0d06
Boris Komraz <bkomraz1@gmail.com>
2017-01-22 13:22:12 +0200

    Gradle build for UI

--- build_all.bash
+++ build_all.bash
@@ -2,10 +2,8 @@ set -e
 
 ( cd ft-analytics && pwd && mvn clean install )
 
-for i in ft-geocoder-facade  ft-gps-service  ft-notifications   ft-device-manager  ft-gps-firehose     ft-management   ft-segments-service ; do
-   ( cd $i && pwd && ./gradlew buildImage )
+for i in ft-geocoder-facade  ft-gps-service  ft-notifications   ft-device-manager  ft-gps-firehose     ft-management   ft-segments-service ft-gps-tracker-ui ; do
+   ( cd $i && pwd && ./gradlew buildImage --info )
 done
-
-( cd ft-gps-tracker-ui  && pwd && docker build . -t shelleg/ft-gps-tracker-ui:latest --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy )
 
 wait
